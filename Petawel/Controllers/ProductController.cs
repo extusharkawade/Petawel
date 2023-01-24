98211521dd997e34e3f165252dcffdfaf09d8fbc
tushar.kawade <tushar.kawade@excellarate.com>
2023-01-24 13:37:15 +0530

    Add project files.

---
+++ Petawel/Controllers/ProductController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Petawel.Controllers.Models;
+
+namespace Petawel.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet]
+        public Response Test()
+        {
+            return new Response();
+        }
+
+    }
+}
