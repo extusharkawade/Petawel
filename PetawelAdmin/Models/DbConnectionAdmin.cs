0092e2985d98577b67b027d144e2478916af580e
Mayuri Toro <63724182+mayuritoro@users.noreply.github.com>
2023-02-08 15:58:49 +0530

    Add files via upload

---
+++ PetawelAdmin/Models/DbConnectionAdmin.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System;
+
+
+namespace PetawelAdmin.Models
+{
+    public class DbConnectionAdmin
+    {
+
+        private readonly IConfiguration _configuration;
+        SqlConnection sqlConnection;
+
+        public DbConnectionAdmin(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            sqlConnection = new SqlConnection(_configuration.GetConnectionString("conn").ToString());
+            Console.WriteLine("sqlconnection Success");
+        }
+
+        public ResponseAdmin getProduct()
+        {
+            Console.WriteLine("sqlconnection Success");
+            ResponseAdmin response = new ResponseAdmin();
+            SqlCommand sqlCommand = new SqlCommand("Select * from products", sqlConnection);
+            Console.WriteLine("sqlconnection", sqlCommand.ToString());
+            sqlConnection.Open();
+
+            SqlDataReader data = sqlCommand.ExecuteReader();
+            //Console.WriteLine("sqlconnection i ", i.ToString());
+            sqlConnection.Close();
+            if (data != null)
+            {
+                response.StatusCode = 200;
+                response.StatusMessage = "Successful";
+            }
+            else
+            {
+                response.StatusCode = 500;
+                response.StatusMessage = "not working";
+            }
+            return response;
+        }
+    }
+}
---
+++ PetawelAdmin/Models/ResponseAdmin.cs
@@ -0,0 +1,9 @@
+﻿namespace PetawelAdmin.Models
+{
+    public class ResponseAdmin
+    {
+
+        public int StatusCode { get; set; }
+        public string StatusMessage { get; set; }
+    }
+}
