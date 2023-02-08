32873f4a6891097906a9dda50e328eb5c710ac87
Mayuri Toro <63724182+mayuritoro@users.noreply.github.com>
2023-02-08 15:53:54 +0530

    Add files via upload

---
+++ PetawelAdmin/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PetawelAdmin.Models;
+using System.Data.SqlClient;
+
+namespace PetawelAdmin.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : Controller
+    {
+        private readonly IConfiguration _configuration;
+       
+        public HomeController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+
+        [HttpGet]
+        [Route("Home")]
+        public ResponseAdmin getProduct()
+        {
+            //Response response = new Response();
+            DbConnectionAdmin dbConnections = new DbConnectionAdmin(_configuration);
+            Console.WriteLine(" in controller sqlconnection");
+            ResponseAdmin response = dbConnections.getProduct();
+
+            return response;
+        }
+    }
+}
